7035d1c4060f8c723e5e89faf080175b6afca62f
milospetrovic <milosp@ultimatemovers.net>
2021-09-30 21:35:41 +0200

    remove submit form js

--- js/script.js
+++ js/script.js
@@ -317,49 +317,4 @@ jQuery(function ($) {
         $(".side-modal").removeClass("side-modal-show");
     })
 
-    // Subscribe form
-    $("#contactForm, .newsletter-form").validator().on("submit", function(event) {
-        if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            formErrorSub();
-            submitMSGSub(false, "Please enter your email correctly.");
-        } else {
-            // everything looks good!
-            event.preventDefault();
-        }
-    });
-    function callbackFunction(resp) {
-        if (resp.result === "success") {
-            formSuccessSub();
-        } else {
-            formErrorSub();
-        }
-    }
-    function formSuccessSub() {
-        $(".newsletter-form")[0].reset();
-        submitMSGSub(true, "Thank you for subscribing!");
-        setTimeout(function() {
-            $("#validator-newsletter").addClass('hide');
-        }, 4000)
-    }
-    function formErrorSub() {
-        $(".newsletter-form").addClass("animate__animated animate__shakeX");
-        setTimeout(function() {
-            $(".newsletter-form").removeClass("animate__animated animate__shakeX");
-        }, 1000)
-    }
-    function submitMSGSub(valid, msg) {
-        if (valid) {
-            var msgClasses = "validation-success";
-        } else {
-            var msgClasses = "validation-danger";
-        }
-        $("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
-    }
-
-    // ajax mailchimp
-    $(".newsletter-form").ajaxChimp({
-        url: "https://hibootstrap.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-        callback: callbackFunction
-    });
 });
