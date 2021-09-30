3ad16b8061bf905e35034be40fd3a50fc6048673
milospetrovic <milosp@ultimatemovers.net>
2021-09-30 22:06:52 +0200

    add contact form API

--- js/script.js
+++ js/script.js
@@ -179,6 +179,28 @@ jQuery(function ($) {
     // Nice select
     $(".product-sort").niceSelect();
 
+    // Contact form
+    var form = document.getElementById("contactForm");
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("msgSubmit");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
     // Product-details-carousel
     var productSync1 = $(".product-slider-for");
     var productSync2 = $(".product-slider-nav");
