3b6a5a39cc228ade56b6281bbdf2dbb6b5b6c7f2
milospetrovic <milosp@ultimatemovers.net>
2021-09-30 11:45:27 +0200

    close menu on click on mobile

--- js/script.js
+++ js/script.js
@@ -18,6 +18,9 @@ jQuery(function ($) {
       jQuery('html, body').animate({
           scrollTop: $('[id="' + $.attr(this, 'href').substr(1) + '"]').offset().top
       }, 500);
+      if(window.innerWidth <= 700) {
+        jQuery('.desktop-nav').removeClass('open')
+      }
 
       return false;
   });
